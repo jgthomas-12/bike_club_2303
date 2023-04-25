3b4a28919b77d8def02a8d78b379c81a96cb3fa3
Jesse Thomas <jgthomas3@gmail.com>
2023-04-25 09:59:10 -0600

    Feat: #log_ride.

--- lib/biker.rb
+++ lib/biker.rb
@@ -14,4 +14,20 @@ class Biker
   def learn_terrain!(terrain)
     @acceptable_terrain << terrain
   end
+
+  def log_ride(ride, time)
+    if @acceptable_terrain.include?(ride.terrain)
+      if @rides[ride] == nil
+        @rides[ride] = [time]
+      else
+        @rides[ride] += [time]
+      end
+    end
+    # rider_log = Hash.new(0)
+    # require 'pry'; binding.pry
+    # if rider_log.keys.include?(ride.name)
+    #   rider_log[ride] = time
+    # end
+
+  end
 end
