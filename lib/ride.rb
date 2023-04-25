94c8ca35abcf0f955da5549af9c012f3e0cbd2b2
Jesse Thomas <jgthomas3@gmail.com>
2023-04-25 09:15:11 -0600

    Feat: Ride class start, loop? method.

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,17 @@
 class Ride
+  attr_reader :name,
+              :distance,
+              :loop,
+              :terrain
 
+  def initialize(details)
+    @name = details[:name]
+    @distance = details[:distance]
+    @loop = details[:loop]
+    @terrain = details[:terrain]
+  end
+
+  def loop?
+    @loop
+  end
 end
