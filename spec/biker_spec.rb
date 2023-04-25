a7cc3565087b887f4b40c9d21242e99a2b25bf34
Jesse Thomas <jgthomas3@gmail.com>
2023-04-25 10:42:59 -0600

    Test: Personal_record and biker 2

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -4,6 +4,7 @@ require './lib/biker'
 RSpec.describe Biker do
   before(:each) do
     @biker = Biker.new("Kenny", 30)
+    @biker2 = Biker.new("Athena", 15)
     @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
     @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
   end
@@ -62,6 +63,42 @@ RSpec.describe Biker do
       }
       expect(@biker.rides).to eq(expected)
     end
+  end
 
+  describe '#personal_record' do
+    it 'returns the best time of a ride key' do
+      @biker.learn_terrain!(:hills)
+      @biker.learn_terrain!(:gravel)
+
+      @biker.log_ride(@ride1, 92.5)
+      @biker.log_ride(@ride1, 91.1)
+      @biker.log_ride(@ride2, 60.9)
+      @biker.log_ride(@ride2, 61.6)
+
+      expect(@biker.personal_record(@ride1)).to eq(91.1)
+      expect(@biker.personal_record(@ride2)).to eq(60.9)
+    end
+  end
+
+  describe 'biker 2' do
+    it 'can do all the things' do
+      @biker2.log_ride(@ride1, 97.0)
+      @biker2.log_ride(@ride2, 67.0)
+
+      expect(@biker2.rides).to eq({})
+      @biker2.learn_terrain!(:gravel)
+      @biker2.learn_terrain!(:hills)
+
+      @biker2.log_ride(@ride1, 95.0)
+      @biker2.log_ride(@ride2, 65.0)
+
+      expected = {
+        @ride2 => [65.0]
+      }
+
+      expect(@biker2.rides).to eq(expected)
+      expect(@biker2.personal_record(@ride2)).to eq(65.0)
+      expect(@biker2.personal_record(@ride1)).to eq(false)
+    end
   end
 end
